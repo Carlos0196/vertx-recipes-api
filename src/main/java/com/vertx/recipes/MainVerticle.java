3093466f7c9a2b2aa2061d590559567ed2e8a13e
Carlos Guzmán <carlos.guzman@alten.es>
2021-03-29 12:59:50 +0200

    Code cleaning

--- src/main/java/com/vertx/recipes/MainVerticle.java
+++ src/main/java/com/vertx/recipes/MainVerticle.java
@@ -1,13 +1,9 @@
 package com.vertx.recipes;
 
 import com.vertx.recipes.controller.RecipeController;
-import com.vertx.recipes.repository.RecipeRepository;
 import com.vertx.recipes.repository.RecipeRepositoryProvider;
 import com.vertx.recipes.service.RecipeServiceProvider;
-import com.vertx.recipes.verticle.RecipeHttpServerVerticle;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
@@ -34,9 +30,6 @@ public class MainVerticle extends AbstractVerticle {
         // Deploy http server
         Router router = Router.router(vertx);
 
-        // CORS
-        // enableCorsSupport(router);
-
         // Nos permitirá parsear el body handler para obtener el payload
         router.route().handler(BodyHandler.create());
 
@@ -46,6 +39,7 @@ public class MainVerticle extends AbstractVerticle {
             context.response().end(checkResult.encode());
         });
 
+        // Register Recipe controller
         router.route(RecipeController.getApiPath()).handler(new RecipeController(router));
 
         // Server config with default data
@@ -60,32 +54,6 @@ public class MainVerticle extends AbstractVerticle {
                 LOGGER.info(" failed to start " + this.getClass().getSimpleName());
             }
         });
-
-        /*deployVerticle()
-            .onSuccess(ar -> {
-                startPromise.complete();
-                System.out.println("HTTP server started on port 8888");
-            })
-            .onFailure(ar -> startPromise.fail(ar.getCause()));*/
     }
 
-    private Future<Void> deployVerticle() {
-        Promise<Void> promise = Promise.promise();
-        DeploymentOptions deploymentOptions = new DeploymentOptions();
-        deploymentOptions.setConfig(config());
-        deploymentOptions.setInstances(Runtime.getRuntime().availableProcessors() * 2);
-
-        // Http server verticle
-        vertx.deployVerticle(RecipeHttpServerVerticle.class.getName(), deploymentOptions, ar -> {
-            if (ar.succeeded()) {
-                LOGGER.info(String.format("Deployment verticle ok RecipeHttpServerVerticle "));
-                promise.complete();
-            } else {
-                LOGGER.info(String.format("Deployment verticle ko RecipeHttpServerVerticle " + ar.cause()));
-                promise.fail(ar.cause());
-            }
-        });
-
-        return promise.future();
-    }
 }
